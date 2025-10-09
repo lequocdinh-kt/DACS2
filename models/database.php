6843d65f4c1495882503c05dcd264232797335b5
Dinh Le <lequocdinh1805@gmail.com>
2025-10-09 07:52:03 +0700

    taoj file hello

---
+++ models/database.php
@@ -0,0 +1,13 @@
+<?php
+$dsn = 'mysql:host=localhost;port=3306;dbname=dacs2';
+$username = 'root';
+$password = '';
+try {
+    $db = new PDO($dsn, $username, $password);
+    echo "Connected to database";
+} catch (PDOException $e) {
+    $error_message = $e->getMessage();
+    include('database_error.php');
+    exit();
+}
+?>
