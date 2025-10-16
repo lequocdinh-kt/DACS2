671442444526d9b196abc3eee8c52f98890fb612
Dinh Le <lequocdinh1805@gmail.com>
2025-10-16 22:13:57 +0700

    a

--- src/models/database.php
+++ src/models/database.php
@@ -1,12 +1,12 @@
 <?php
-$dsn = 'mysql:host=onehost-webhn072403.000nethost.com;port=3306;dbname=slrnkpifhosting_DACS2;charset=utf8';
+$dsn = 'mysql:host=localhost;port=3306;dbname=slrnkpifhosting_DACS2;charset=utf8';
 $username = 'slrnkpifhosting_xiaoying';
-$password = 'i[~+5xn4?eyX4VX'; // thay bằng mật khẩu thật trong OnePanel
+$password = 'i[~+5xn4?eyX4VX'; // mật khẩu thật của user MySQL
 
 try {
     $db = new PDO($dsn, $username, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "✅ Kết nối thành công!";
+    echo "✅ Kết nối thành công!";
 } catch (PDOException $e) {
     echo "❌ Lỗi kết nối CSDL: " . $e->getMessage();
     exit();
