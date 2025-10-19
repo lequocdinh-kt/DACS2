848bfa32623791f3e2d1b479e535580f9864a607
Dinh Le <lequocdinh1805@gmail.com>
2025-10-19 17:20:45 +0700

    index

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="src/styles/footer.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
-    <title>Document</title>
+    <title>Home</title>
 </head>
 
 <body>
